527a705c6719d001d534b8b7e9f0781a006b4279
sergozheludkov <sergozheludkov@yandex.ru>
2024-06-22 07:22:32 +0300

    MB-0071 | buttons guide

--- packages/bot/src/scenes/accession/accession.tsx
+++ packages/bot/src/scenes/accession/accession.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Text } from '@urban-bot/core';
+import { Text, Image } from '@urban-bot/core';
 import { useTranslation } from '@common_bot/i18n';
+
+const IMAGE_ID = 'AgACAgIAAxkBAAI7HWZ2SR0rgLYCOeYKW0qs8k6Rh22QAAIG2zEb2wawSwaR7ZVe5dLdAQADAgADcwADNQQ';
 
 export const Accession = () => {
   const { t } = useTranslation('rules');
 
-  // TODO добавить сообщение про потерю кнопок
-
   return (
-    <Text>
-      <b>{t('rules:title')}</b>
-      <br />
-      <br />
-      -
-      &#32;
-      {t('rules:multi_accounting')}
-      <br />
-      -
-      &#32;
-      {t('rules:fraud')}
-    </Text>
+    <>
+      <Text>
+        <b>{t('rules:title')}</b>
+        <br />
+        <br />
+        -
+        &#32;
+        {t('rules:multi_accounting')}
+        <br />
+        -
+        &#32;
+        {t('rules:fraud')}
+      </Text>
+      <Image file={IMAGE_ID} />
+    </>
   );
 };
